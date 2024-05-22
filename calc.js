e3f0a4a57c51064d549df83c3237ed5713709d8d
Georgi Nenchev-PC <g.nenchev89@gmail.com>
2024-05-22 16:21:03 +0300

    bug fixed with operator buttons

--- calc.js
+++ calc.js
@@ -1,55 +1,28 @@
-const butons = document.querySelectorAll('.btn');
 const display = document.querySelector('#digits')
 
 export function getValue(e) {
+
+    let input = e.target.value;
+
+    if(e.target.tagName == 'BUTTON') {
+
+        if(input == 'clear') {
+            display.textContent = '';
+        }
+        if(input == '=') {
+            let result = eval(display.textContent);
+            display.textContent = result;
+        }
     
-    let currentBtn = e.target.value;
-    let nums = '';
-    let operator = '';
-
-    if(currentBtn == '') {
-        display.textContent = '';
-        nums = '';
-        operator = '';
+        if(Number(input) || input == '.' || input == '0') {
+            if(display.textContent.length == 0 && input == '.') {
+                display.textContent += `0${input}`
+            } else {
+                display.textContent += input
+            }
+        }else if(display.textContent.length > 0 && input !== '=') {
+            display.textContent += input
+        }
     }
-
-    if(currentBtn !== undefined) {
-        if(currentBtn !== '+' && currentBtn !== '-' && currentBtn !== '*' && currentBtn !== '/' && currentBtn !== '=') {
-            nums += e.target.value;
-            display.textContent += nums;
-        }
-        if(currentBtn == '+' || currentBtn == '-' || currentBtn == '*' || currentBtn == '/') {
-            operator = currentBtn;
-            display.textContent += nums + ' ' + operator + ' ';
-        }
-        if(currentBtn == '=') {
-            
-            let finalResult = 0;
-            let expression = display.textContent;
-            let [firstNum, op, secondNum] = expression.split(' ');
-
-            const operators = {
-                '-': calcMinusResult(firstNum, secondNum),
-                '+': calcPlusResult(firstNum, secondNum),
-                '*': calcMultyplyResult(firstNum, secondNum),
-                '/': calcDivadeResult(firstNum, secondNum)
-            } 
-
-            finalResult = operators[op];
-            display.textContent = finalResult;
-        }
-    } 
-}
-
-function calcMultyplyResult(numA, numB) {
-    return (Number(numA) * Number(numB))
-}
-function calcMinusResult(numA, numB) {
-    return (Number(numA) - Number(numB))
-}
-function calcPlusResult(numA, numB) {
-    return (Number(numA) + Number(numB))
-}
-function calcDivadeResult(numA, numB) {
-    return (Number(numA) / Number(numB))
+      
 }
